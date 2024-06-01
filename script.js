e00127984cabff4af1ea4ed1758ad871343c3918
gpswazir <gpswazir@gmail.com>
2024-06-01 22:50:27 +0530

    rows and clumns

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 const container = document.querySelector("#container");
 
+function columnIndexToLetter(index){
+    return String.fromCharCode(64 + index);
+}
 
-for(let row =1; row <= 25; row++){
+const columnHeadRow = document.createElement("div");
+for(let i = 0; i <= 26; i++){
+    const headCell = document.createElement("div");
+    headCell.className = `col`;
+    if(i > 0){
+        headCell.textContent = columnIndexToLetter(i);
+    }
+    else{
+        headCell.textContent = "00"
+    }
+    
+    columnHeadRow.append(headCell);
+}
+container.append(columnHeadRow);
+
+
+
+
+for(let row =1; row <= 26; row++){
     const rowElement = document.createElement("div");
-    rowElement.className = `sdafw`;
+    
+    const rowHeader  = document.createElement("div");
+    rowHeader.className = `col`;
+    rowHeader.textContent = `${row}`;
+    rowElement.append(rowHeader);
+    
 
     for(let col = 1; col <= 26; col++){
         const cellElement = document.createElement("div");
         cellElement.className = `col`;
-        
+              
         //col.setAttribute("contenEditable", "true")
         cellElement.contentEditable = "true";
-
-        rowElement.appendChild(cellElement);
+        cellElement.innerHTML = `${col} ${row}`;
+         rowElement.append(cellElement);
      
     }
     container.append(rowElement);
